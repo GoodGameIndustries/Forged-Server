6d18a85e8a04ebe2fa1b2ec7c6c52d91cd4a1c7d
EmmettGGI <goodgameindustries@gmail.com>
2014-11-28 21:39:45 -0500

    Setup Libgdx Project

---
+++ desktop/src/com/GGI/ForgedServer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.GGI.ForgedServer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.GGI.ForgedServer.ForgedServer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ForgedServer(), config);
+	}
+}
---
+++ html/src/com/GGI/ForgedServer/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.GGI.ForgedServer.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.GGI.ForgedServer.ForgedServer;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ForgedServer();
+        }
+}
